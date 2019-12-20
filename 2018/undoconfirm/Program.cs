cf0cf77552137f38cff729730a10820eed6dee8e
Andrey Rodionov <roand@inbox.ru>
2019-12-20 10:37:04 +0300

    Добавляет проект разутверждения техпроцесса

---
+++ 2018/undoconfirm/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Windows.Forms;
+
+using Ascon.Vertical;
+using Ascon.Vertical.Technology;
+using Ascon.Integration;
+namespace undoconfirm{
+	internal sealed class Program
+	{
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			try {
+				AuthenticationManager.Authenticate();			
+			} catch (Exception) {
+				MessageBox.Show("Ошибка авторизации!");
+				return;
+			}
+
+                        OpenFileDialog openFileDialog1 = new OpenFileDialog  
+                        {  
+                            InitialDirectory = @"c:\",  
+                            Title = "Выбор файла техпроцесса",  
+  
+                            CheckFileExists = true,  
+                            CheckPathExists = true,  
+  
+                            DefaultExt = "vtp",  
+                            Filter = "Файлы ТП (*.vtp;*.ttp)|*.vtp;*.ttp",  
+                            FilterIndex = 4,  
+                            RestoreDirectory = true,  
+  
+                            ReadOnlyChecked = true,  
+                            ShowReadOnly = true  
+                        };  
+                        
+                        if (openFileDialog1.ShowDialog() != DialogResult.OK)  
+                        {  
+                            AuthenticationManager.Deauthenticate();
+                            return;   
+                        }  
+                           
+                        TechDocument doc = null;
+                        try {
+			    doc = TechDocument.Load(openFileDialog1.FileName);
+			 } catch (Exception) {
+                            doc.Close();
+    			    AuthenticationManager.Deauthenticate();
+		  	    MessageBox.Show("Ошибка загрузки техпроцесса!");
+			    return;
+			}
+
+                        if (doc.Extensions.EngineeringChangesManager.State 
+                            == TechEngineeringChangesTechnologyState.Approved) 
+                        {
+                            doc.Extensions.EngineeringChangesManager.CancelLatestApprove();
+                            doc.Save( openFileDialog1.FileName);
+                        } else {
+                            MessageBox.Show("Техпроцесс не утвержден!");
+                        }  
+                       
+			doc.Close();
+			AuthenticationManager.Deauthenticate();
+			MessageBox.Show("Выполнено!");				
+		}
+	}
+}
---
+++ 2018/undoconfirm/build.cmd
@@ -0,0 +1,32 @@
+@echo off
+
+rem Собранный файл необходимо запускать из каталога Вертикаль
+
+rem Список библиотек Вертикаль
+set libs=Ascon.Vertical.Technology.dll
+
+rem Наименование программы
+set out=undoconfirm
+
+rem Версия компилятора
+set csc_ver=v4.0
+
+
+
+for /f "delims=" %%a in ('where /R %windir%\Microsoft.NET\assembly\GAC_MSIL\Ascon.Integration Ascon.Integration.dll') do set "refs=%%a"
+for /f "delims=" %%a in ('where /R %windir%\Microsoft.NET\Framework64\ csc.exe ^| findstr %csc_ver%') do set "CSC=%%a"
+
+set VRT=%ProgramFiles%\ASCON\Vertical
+IF EXIST "%VRT%" GOTO EXISTDIR
+  set VRT=%ProgramFiles(x86)%\ASCON\Vertical
+
+:EXISTDIR
+
+for %%i in (%libs%) do (
+set refs=%refs%;"%VRT%\%%i"
+)
+
+%CSC% /out:%out%.exe /R:%refs% *.cs
+
+
+
